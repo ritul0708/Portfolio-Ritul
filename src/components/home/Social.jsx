b617813f4035d53c37075e08c1155336ae35c63f
Ritul Jain <rituljain0708@gmail.com>
2023-02-16 12:07:23 +0530

    home page done

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://www.linkedin.com/in/ritul0708/" 
+        className="home__social-icon" target="_blank">
+        <i className="uil uil-linkedin"></i>
+      </a>
+
+      <a href="https://github.com/ritul0708" 
+        className="home__social-icon" target="_blank">
+        <i className="uil uil-github-alt"></i>
+      </a>
+
+      <a href="https://www.instagram.com/rituljain0708/" 
+        className="home__social-icon" target="_blank">
+      < i className="uil uil-instagram"></i>
+      </a>
+    </div>
+  )
+}
+
+export default Social
