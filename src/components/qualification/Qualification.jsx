9f248c2ad074cb51b6313caaff2789f8e119ba4b
Ritul Jain <rituljain0708@gmail.com>
2023-02-21 16:13:24 +0530

    qualification page -toggling done

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './qualification.css';
 
 const Qualification = () => {
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = index => {
+    setToggleState(index)
+  }
+
+
+
+
   return (
     <div className="qualification section">
       <h2 className="section__title">Qualification</h2>
@@ -9,23 +18,43 @@ const Qualification = () => {
 
       <div className="qualification__container container">
         <div className="qualification__tabs">
-          <div className="qualification__button button--flex qualification__active">
+          <div 
+            className={
+              toggleState === 1 
+              ? "qualification__button button--flex qualification__active"
+              : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(1)}
+          >
             <i className="uil uil-graduation-cap qualification-icon"></i>{" "}
             Experience
           </div>
 
-          <div className="qualification__button button--flex">
+          <div 
+            className={
+              toggleState === 2
+              ? "qualification__button button--flex qualification__active"
+              : "qualification__button button--flex"
+            }
+            onClick={() => toggleTab(2)}
+          >
             <i className="uil uil-briefcase-alt qualification-icon"></i>{" "}
             Education
           </div>
         </div>
 
         <div className="qualification__sections">
-          <div className="qualification__content">
+        <div 
+            className={
+              toggleState === 2
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
             <div className="qualification__data">
 
               <div>
-                <h3 className="qualification__title"></h3>
+                <h3 className="qualification__title">EIC, B.Tech.</h3>
                 <span className="qualification__subtitle">RTU, Kota</span>
                 <div className='qualification__calendar'>
                   <i className="uil uil-calendar-alt"></i>
@@ -48,7 +77,7 @@ const Qualification = () => {
               </div> 
 
               <div>
-                <h3 className="qualification__title"></h3>
+                <h3 className="qualification__title">12th</h3>
                 <span className="qualification__subtitle">Maa Bharti School</span>
                 <div className='qualification__calendar'>
                   <i className="uil uil-calendar-alt"></i>
@@ -60,7 +89,13 @@ const Qualification = () => {
 
           </div>
 
-          <div className="qualification__content qualification__content-active">
+          <div 
+            className={
+              toggleState === 1
+                ? "qualification__content qualification__content-active"
+                : "qualification__content"
+            }
+          >
             <div className="qualification__data">
 
               <div>
@@ -80,6 +115,8 @@ const Qualification = () => {
 
 
           </div>
+
+
         </div>
       </div>
     </div>
